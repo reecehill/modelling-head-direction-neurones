361b2e1cc758f69eda1f31634ee7d2fe8ffdad96
Reece Hill <29638128+reecehill@users.noreply.github.com>
2022-04-25 13:01:34 +0100

    move weight generation into Neurone class

--- Simulation/classes/NeuronalPopulation.py
+++ Simulation/classes/NeuronalPopulation.py
@@ -3,19 +3,27 @@ import weightHandler as wh
 import numpy as np
 from classes.Neurone import Neurone
 
-class NeuronalPopulation:
-  def __init__(self):
-      # It is possible to generate neurones in one of two ways. See parameters.py for more information.
-      if(p.generateWeightsMethod == 'independentNeurone'):
-        # Get each neurone's weight independently by looping through all theta values.
-        self.neurones = np.array([Neurone(theta_0=theta, evenWeights=wh.generateWeightsForOneNeurone(
-            theta_0=theta)) for theta in p.thetaSeries])
-        
-      elif(p.generateWeightsMethod == 'templateNeurone'):
-      # Instantiate a neurone with preferred direction 0, then roll its weights to offset them according to thetaIndex.
-        self.neurones = np.array([Neurone(theta_0=theta, evenWeights=np.roll(wh.generateWeightsForOneNeurone(theta_0=p.actualTheta), thetaIndex)) for thetaIndex, theta in enumerate(p.thetaSeries)])
 
-  def getAllWeights(self):
-    # Returns w(θ,t)
-    # See: Equation 2. 
-    return np.array(np.add([neurone.evenWeights for neurone in self.neurones],[neurone.oddWeights for neurone in self.neurones]))
+class NeuronalPopulation:
+    def __init__(self):
+        # It is possible to generate neurones in one of two ways. See parameters.py for more information.
+        if(p.generateWeightsMethod == 'independentNeurone'):
+            # Get each neurone's weight independently by looping through all theta values.
+            
+            self.neurones = np.array([Neurone(theta_0=theta) for theta in p.thetaSeries])
+            print("Weights generated")
+
+        elif(p.generateWeightsMethod == 'templateNeurone'):
+            # Instantiate a neurone with preferred direction 0, then roll its weights to offset them according to thetaIndex.
+            self.neurones = np.array([Neurone(theta_0=0) for theta in p.thetaSeries])
+            # TODO: This function, np.roll, has not been tested here. Check output if using templateNeurone.
+            self.neurones = [np.roll(neurone.evenWeights, neuroneIndex) for neuroneIndex, neurone in enumerate(self.neurones)]
+            
+
+    def getAllWeights(self):
+        # Returns w(θ,t)
+        # See: Equation 2.
+        evenWeights = [neurone.evenWeights for neurone in self.neurones]
+        oddWeights = [neurone.oddWeights for neurone in self.neurones]
+        summedWeights = np.add(evenWeights, oddWeights)
+        return np.array(summedWeights)
--- Simulation/classes/Neurone.py
+++ Simulation/classes/Neurone.py
@@ -1,17 +1,20 @@
 import numpy as np
 import mathEquations as e
 import parameters as p
-
+import weightHandler as wh
 class Neurone:
-  def __init__(self, theta_0, evenWeights):
+  def __init__(self, theta_0):
     self.theta_0 = theta_0
-    self.evenWeights = evenWeights
-    self.oddWeights = np.zeros(evenWeights.shape)
     self.tuningCurve = e.getTuningCurve(theta_0=theta_0)
+    self.evenWeights = wh.generateWeightsForOneNeurone(neurone=self)
+    self.oddWeights = np.zeros(self.evenWeights.shape)
+    
     
     # Create a matrix of zeros. The first dimension is the time (t), and the second are the weights (even and odd), the third are respective matrices. 
-    self.firingActivity = np.zeros((len(p.timeSeries), 2, evenWeights.size))
+    self.firingActivity = np.zeros(
+        (len(p.timeSeries), 2, self.evenWeights.size))
     self.firingActivity[0][0] = self.evenWeights
     self.firingActivity[0][1] = self.oddWeights
+
   def getWeights(self):
     return self.evenWeights + self.oddWeights
